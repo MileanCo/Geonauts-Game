faf583b109d7cf174f169e785bbd5afe4dfa3d71
Joel Stenquist <jmstenkv@ncsu.edu>
2014-04-16 01:46:46 -0400

    Added explosionHit and explosion06. Also fixed size for explosion11.

--- src/com/me/geonauts/model/entities/anims/Explosion06.java
+++ src/com/me/geonauts/model/entities/anims/Explosion06.java
@@ -3,13 +3,13 @@ package com.me.geonauts.model.entities.anims;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.math.Vector2;
 
-public class Explosion11 extends AbstractAnimation {
+public class Explosion06 extends AbstractAnimation {
 
-	private static Vector2 SIZE = new Vector2((120/64f), (120/60f));
+	private static Vector2 SIZE = new Vector2((64/64f), (64/60f));
 	
 	public static Animation anim;
 	
-	public Explosion11(Vector2 pos, float scl) {
+	public Explosion06(Vector2 pos, float scl) {
 		super(pos, SIZE.cpy().scl(scl));
 		
 	}
