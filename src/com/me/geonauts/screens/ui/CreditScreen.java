f254b3c0599390e47703d97940595da200d90b26
ryanjamar <ryanjamar@ubuntu>
2014-04-17 14:58:46 -0400

    ui

---
+++ src/com/me/geonauts/screens/ui/CreditScreen.java
@@ -0,0 +1,12 @@
+package com.me.geonauts.screens.ui;
+
+import com.badlogic.gdx.Game;
+
+public class CreditScreen extends AbstractScreen{
+
+	public CreditScreen(Game game) {
+		super(game);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/com/me/geonauts/screens/ui/OptionsScreen.java
@@ -0,0 +1,12 @@
+package com.me.geonauts.screens.ui;
+
+import com.badlogic.gdx.Game;
+
+public class OptionsScreen extends AbstractScreen{
+
+	public OptionsScreen(Game game) {
+		super(game);
+		// TODO Auto-generated constructor stub
+	}
+
+}
